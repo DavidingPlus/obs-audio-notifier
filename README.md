ab614399f5c049ece701aa5153e2dcac89a8eab5
DavidingPlus <davidingplus@qq.com>
2024-11-13 17:55:57 +0800

    gc

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # obs-audio-notifier
 
-An audio notifier script for OBS Studio.
+An audio notifier script for OBS Studio that plays sounds when the replay buffer is saved.
 
--- obs-audio-notifier.lua
+++ obs-audio-notifier.lua
@@ -8,30 +8,36 @@ local SND_FILENAME = 0x00020000
 local fdwSound_flags = SND_ASYNC + SND_NODEFAULT + SND_FILENAME
 local fdwSound_flags_looped = SND_ASYNC + SND_NODEFAULT + SND_LOOP + SND_FILENAME
 
--- Put a sounds of your choice in directory res and don't forget to match its names either in code below or rename your existing files.
+-- Sound file path
 SOUND_REPLAY_BUFFER_SAVED_PATH = script_path() .. "./res/replay-buffer-saved.wav"
 
 ffi.cdef[[
-    bool PlaySound(const char *pszSound, void *hmod, uint32_t fdwSound);
+  bool PlaySound(const char *pszSound, void *hmod, uint32_t fdwSound);
 ]]
 
 function playsound(filepath)
-    winmm.PlaySound(filepath, nil, fdwSound_flags)
+  winmm.PlaySound(filepath, nil, fdwSound_flags)
 end
 
 function playsound_looped(filepath)
-    winmm.PlaySound(filepath, nil, fdwSound_flags_looped)
+  winmm.PlaySound(filepath, nil, fdwSound_flags_looped)
 end
 
 function on_event(event)
-  -- see more event: https://docs.obsproject.com/reference-frontend-api
+  -- 回放保存的时候播放音效提醒用户。
   if event == obs.OBS_FRONTEND_EVENT_REPLAY_BUFFER_SAVED then
     playsound(SOUND_REPLAY_BUFFER_SAVED_PATH)
+
+  -- 当 OBS 退出的时候关闭回放缓存功能。（没有 OBS 打开的 API，也很容易理解）
+  elseif event == obs.OBS_FRONTEND_EVENT_EXIT then
+    if obs.obs_frontend_replay_buffer_active() then
+      obs.obs_frontend_replay_buffer_stop()
+    end
   end
 end
 
 function script_description()
-    return "An audio notifier script for OBS Studio."
+  return "An audio notifier script for OBS Studio that plays sounds when the replay buffer is saved."
 end
 
 function script_load(settings)
